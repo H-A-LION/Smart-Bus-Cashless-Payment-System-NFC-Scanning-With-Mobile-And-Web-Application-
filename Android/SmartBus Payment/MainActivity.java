cef1e79e1b8bbe04762c88420b6e943da465f11e
Hussein Azzam <72210080@students.liu.edu.lb>
2025-03-25 16:41:40 +0200

    Update MainActivity.java

--- Android/SmartBus Payment/MainActivity.java
+++ Android/SmartBus Payment/MainActivity.java
@@ -1,17 +1,28 @@
 package com.example.smartbuspayment;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.PatternMatcher;
+import android.util.Patterns;
+import android.widget.Button;
 import android.widget.EditText;
-
-//import androidx.activity.EdgeToEdge;
+import android.widget.Toast;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class MainActivity extends AppCompatActivity {
-    EditText nameEditText;
-    EditText passwordEditText;
+    private FirebaseAuth auth;
+    private EditText nameEditText,passwordEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,43 +34,42 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-
     }
-    public boolean Login(){
+    public void login(){
+        auth=FirebaseAuth.getInstance();
         nameEditText =(EditText) findViewById(R.id.name_login_editText);
         passwordEditText =(EditText) findViewById(R.id.password_login_editText);
-        String name=nameEditText.getText().toString();
-        String password=passwordEditText.getText().toString();
-        AccountHandler account=new AccountHandler(name,password);
+        String User=nameEditText.getText().toString().trim();
+        String password=passwordEditText.getText().toString().trim();
 
+        if (!User.isEmpty()){
+            if (!password.isEmpty()){
+                auth.signInWithEmailAndPassword(User,password)
+                        .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                            @Override
+                            public void onSuccess(AuthResult authResult) {
+                                Toast.makeText(MainActivity.this,"Login Successful",Toast.LENGTH_LONG).show();
+                                startActivity(new Intent(MainActivity.this,MainActivity2.class));
+                                finish();
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(MainActivity.this,"Login Failed",Toast.LENGTH_LONG).show();
+                            }
+                        });
 
-
-        return false;
+            }
+        }else{
+            if (User.isEmpty()) {
+                nameEditText.setError("Email cannot be Empty!!");
+            }
+            if (password.isEmpty()) {
+                passwordEditText.setError("Password cannot be Empty");
+            }
+        }
     }
-    private class AccountHandler{
-        String name;
-        String password;
-
-        public AccountHandler(String name , String password) {
-            this.name = name;
-            this.password=password;
-        }
-
-        public String getName() {
-            return name;
-        }
-
-        public void setName(String name) {
-            this.name = name;
-        }
-
-        public String getPassword() {
-            return password;
-        }
-
-        public void setPassword(String password) {
-            this.password = password;
-        }
+    public void signup(){
+        startActivity(new Intent(MainActivity.this, SignUpActivity.class));
     }
 }
